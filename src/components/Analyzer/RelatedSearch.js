97cc9a0301e9e03cc47df68400f9e127f2ce09a0
Andrew Tam <andrew.tam.000@gmail.com>
2018-09-22 10:12:22 -0400

    Added compoents

---
+++ src/components/Analyzer/RelatedSearch.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Drawer from "@material-ui/core/Drawer";
+
+const data = [];
+
+class RelatedSearch extends Component {
+    render() {
+        return <div>hi</div>;
+    }
+}
+
+export default RelatedSearch;
---
+++ src/components/Analyzer/SearchButton.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { reduce, set, get } from "lodash";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
+import { compose, mapProps } from "recompose";
+import Search from "@material-ui/icons/Search";
+import Button from "@material-ui/core/Button";
+import { songsWithDataByIdSelector } from "../../selectors";
+import { advancedSearchAddTrack } from "../../redux/actions";
+import { tableConfig } from "./SongTable";
+
+const SearchButton = ({ advancedSearchAddTrack, trackDetails }) => (
+    <Button
+        onClick={() => advancedSearchAddTrack(trackDetails)}
+        mini
+        variant="fab"
+        color="primary"
+        aria-label="Add"
+    >
+        <Search />
+    </Button>
+);
+
+const mapStateToProps = createStructuredSelector({
+    songsWithDataById: songsWithDataByIdSelector
+});
+
+const mapDispatchToProps = { advancedSearchAddTrack };
+
+export default compose(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    ),
+    mapProps(({ songsWithDataById, id, ...props }) => ({
+        trackDetails: reduce(
+            tableConfig,
+            (acc, config) => set(acc, config.dataKey, get(songsWithDataById[id], config.getter())),
+            {}
+        ),
+        ...props
+    }))
+)(SearchButton);
