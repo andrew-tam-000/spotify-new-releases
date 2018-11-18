7a0433e080c1c18ea14ffca27928a97e7d230b6e
Andrew Tam <andrew.tam.000@gmail.com>
2018-11-18 00:43:59 -0500

    Adding sliders

--- src/components/Table/ItemTagList.js
+++ src/components/Table/ItemTagList.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import TagProvider from "../Table/TagProvider";
-import React from "react";
+import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
-import { map } from "lodash";
+import { map, keys } from "lodash";
 import { connect } from "react-redux";
 import { compose, withProps } from "recompact";
 import { openNewReleaseModal, setNewReleaseModalGenre } from "../../redux/actions";
+import _ChevronLeft from "@material-ui/icons/ChevronLeft";
+import _ChevronRight from "@material-ui/icons/ChevronRight";
 
 const Tag = styled.span`
     background-color: ${props => props.color};
@@ -21,6 +23,8 @@ const Genres = styled.div`
     display: flex;
     flex-wrap: nowrap;
     overflow: auto;
+    margin-left: 20px;
+    margin-right: 20px;
 `;
 
 const withOpenModal = compose(
@@ -38,21 +42,70 @@ const withOpenModal = compose(
 );
 
 const TagWithOpenModal = withOpenModal(Tag);
+const ChevronRight = styled(_ChevronRight)`
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+
+const ChevronLeft = styled(_ChevronLeft)`
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+
+const ItemTagListWrapper = styled.div`
+    position: relative;
+`;
+
+class GenresWithScroll extends Component {
+    scrollLeft = e => {
+        e.stopPropagation();
+        const currentTarget = e.currentTarget;
+        const parent = currentTarget.parentElement.firstElementChild;
+        const scroll = parent.scrollLeft;
+        parent.scrollLeft -= 200;
+    };
+
+    scrollRight = e => {
+        e.stopPropagation();
+        const currentTarget = e.currentTarget;
+        const parent = currentTarget.parentElement.firstElementChild;
+        const scroll = parent.scrollLeft;
+        parent.scrollLeft += 200;
+    };
+
+    render() {
+        const { genres } = this.props;
+        return (
+            <React.Fragment>
+                <Genres>
+                    {map(genres, genre => (
+                        <TagProvider id={genre}>
+                            {({ active, onClick, color }) => (
+                                <TagWithOpenModal
+                                    genre={genre}
+                                    color={active ? color || "transparent" : undefined}
+                                >
+                                    <Typography variant="caption">{genre}</Typography>
+                                </TagWithOpenModal>
+                            )}
+                        </TagProvider>
+                    ))}
+                </Genres>
+                <ChevronLeft color="action" onClick={this.scrollLeft} />
+                <ChevronRight color="action" onClick={this.scrollRight} />
+            </React.Fragment>
+        );
+    }
+}
 
 const ItemTagList = ({ className, genres }) => (
-    <Genres className={className}>
-        {map(genres, genre => (
-            <TagProvider id={genre}>
-                {({ active, onClick, color }) => (
-                    <TagWithOpenModal genre={genre} color={active ? color || "transparent" : undefined}>
-                        <Typography variant="caption">
-                            {genre}
-                        </Typography>
-                    </TagWithOpenModal>
-                )}
-            </TagProvider>
-        ))}
-    </Genres>
+    <ItemTagListWrapper className={className}>
+        <GenresWithScroll genres={genres} />
+    </ItemTagListWrapper>
 );
 
 export default ItemTagList;
