09bcc9c930f876e84bd61e67c52f0a3d398b332e
Andrew Tam <andrew.tam.000@gmail.com>
2019-01-20 16:41:19 -0500

    Added snack bar for adding songs

--- src/components/Table/AddToLibrary.js
+++ src/components/Table/AddToLibrary.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { compose, withProps } from "recompact";
 import { createStructuredSelector } from "reselect";
@@ -6,9 +6,54 @@ import _AddIcon from "@material-ui/icons/Add";
 import materialStyled from "../../materialStyled";
 import { addToMySavedTracksStart } from "../../redux/actions";
 import { accessTokenSelector } from "../../selectors";
+import Snackbar from "@material-ui/core/Snackbar";
+import _SnackbarContent from "@material-ui/core/SnackbarContent";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
 const AddIcon = materialStyled(_AddIcon)({ cursor: "pointer" });
-const AddToLibrary = compose(
+
+const SnackbarContent = withStyles(theme => ({
+    root: {
+        backgroundColor: theme.palette.primary.dark
+    }
+}))(_SnackbarContent);
+
+class AddToLibrary extends Component {
+    state = { open: false };
+
+    handleClick = e => {
+        e.stopPropagation();
+        this.setState({ open: true });
+        //this.props.onClick();
+    };
+
+    handleClose = () => this.setState({ open: false });
+
+    render() {
+        const { accessToken, fontSize, color } = this.props;
+        return (
+            accessToken && (
+                <React.Fragment>
+                    <AddIcon fontSize={fontSize} color={color} onClick={this.handleClick} />
+                    <Snackbar
+                        open={this.state.open}
+                        autoHideDuration={2000}
+                        onClose={this.handleClose}
+                        anchorOrigin={{
+                            vertical: "bottom",
+                            horizontal: "center"
+                        }}
+                    >
+                        <SnackbarContent message={<Typography>Song added!</Typography>} />
+                    </Snackbar>
+                </React.Fragment>
+            )
+        );
+    }
+}
+
+export default compose(
     connect(
         createStructuredSelector({
             accessToken: accessTokenSelector
@@ -18,8 +63,4 @@ const AddToLibrary = compose(
     withProps(({ addToMySavedTracksStart, id }) => ({
         onClick: () => addToMySavedTracksStart([id])
     }))
-)(function _AddIcon({ accessToken, ...props }) {
-    return accessToken && <AddIcon {...props} />;
-});
-
-export default AddToLibrary;
+)(AddToLibrary);
